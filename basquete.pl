182f10bc0066007b49154aa000c0b1d6a3604bf2
Julia <104947060+juliamacc@users.noreply.github.com>
2023-11-17 16:05:53 -0300

    add tabela e opçoes

---
+++ basquete.pl
@@ -0,0 +1,35 @@
+munhequeira(azul).
+munhequeira(branca).
+munhequeira(verde).
+munhequeira(vermelha).
+munhequeira(preta).
+
+nome(anderson).
+nome(bruno).
+nome(leandro).
+nome(marcelo).
+nome(tiago).
+
+camisa(6).
+camisa(8).
+camisa(23).
+camisa(32).
+camisa(45).
+
+altura(1.90).
+altura(1.95).
+altura(2.00).
+altura(2.05).
+altura(2.10).
+
+idade(20).
+idade(23).
+idade(26).
+idade(29).
+idade(31).
+
+estado(acre).
+estado(mato grosso do sul).
+estado(minas gerais).
+estado(parana).
+estado(pernambuco).
