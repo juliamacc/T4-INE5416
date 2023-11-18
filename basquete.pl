613e00959f15aae255a7d81d8c0ede68af697bf4
Julia <macedo.julia02@gmail.com>
2023-11-18 14:09:34 -0300

    completado

--- basquete.pl
+++ basquete.pl
@@ -29,8 +29,8 @@ idade(29).
 idade(31).
 
 estado(acre).
-estado(mato grosso do sul).
-estado(minas gerais).
+estado(matoGrossoDoSul).
+estado(minasGerais).
 estado(parana).
 estado(pernambuco).
 
@@ -49,6 +49,9 @@ aDireita(X,Y,Lista) :- aEsquerda(Y,X,Lista).
 naBorda(X,Lista) :- last(Lista,X).
 naBorda(X,[X|_]).
 
+% X está entre Y e Z
+entre(X,Y,Z,Lista) :- aDireita(X,Y,Lista), aEsquerda(X,Z,Lista).
+
 todosDiferentes([]).
 todosDiferentes([H|T]) :- not(member(H,T)), todosDiferentes(T).
 
@@ -66,13 +69,13 @@ solucao(ListaSolucao) :-
     Estado3 = parana,
 
     % Tiago está exatamente à esquerda do jogador de 23 anos.
-
+    aoLado(jogador(_,tiago,_,_,_,_), jogador(_,_,_,_,23,_), ListaSolucao),
 
     % Em uma das pontas está o jogador que está usando a munhequeira Branca.
-
+    naBorda(jogador(branca,_,_,_,_,_), ListaSolucao),
 
     % O jogador de 23 anos está ao lado do jogador da munhequeira Verde.
-
+    aoLado(jogador(_,_,_,_,23,_), jogador(verde,_,_,_,_,_), ListaSolucao),
 
     % Marcelo está na quarta posição.
     Nome4 = marcelo,
@@ -81,43 +84,62 @@ solucao(ListaSolucao) :-
     Estado4 = pernambuco,
 
     % O jogador de munhequeira Amarela está exatamente à direita do jogador de munhequeira Verde.
-
+    aDireita(jogador(verde,_,_,_,_,_), jogador(amarela,_,_,_,_,_), ListaSolucao),
 
     % O camisa 8 está em algum lugar entre os camisas 45 e 23, nessa ordem.
-    
+    entre(jogador(_,_,8,_,_,_), jogador(_,_,45,_,_,_), jogador(_,_,23,_,_,_), ListaSolucao),
     
     % O jogador mais baixo está exatamente à esquerda do jogador mais velho.
-    
+    aoLado(jogador(_,_,_,1.90,_,_), jogador(_,_,_,_,31,_), ListaSolucao),
     
     % O jogador de 26 anos está exatamente à direita do jogador de 2,05 m.
-
+    aoLado(jogador(_,_,_,2.05,_,_), jogador(_,_,_,_,26,_), ListaSolucao),
 
     % O Paranaense usa a camisa 32.
-
+    member(jogador(_,_,32,_,_,parana), ListaSolucao),
 
     % Na quarta posição está o jogador de 31 anos.
     Idade4 = 31,
 
     % O jogador que nasceu no Acre está ao lado do jogador de 2,05 m.
-
+    aoLado(jogador(_,_,_,2.05,_,_), jogador(_,_,_,_,_,acre), ListaSolucao),
 
     % O jogador de 2,00 m está ao lado do jogador que é 10 cm menor que ele.
-
+    aoLado(jogador(_,_,_,2.00,_,_), jogador(_,_,_,1.90,_,_), ListaSolucao),
 
     % Marcelo está em algum lugar entre o jogador de 2,05 m e o Bruno, nessa ordem.
-
+    entre(jogador(_,marcelo,_,_,_,_), jogador(_,_,_,2.05,_,_), jogador(_,bruno,_,_,_,_), ListaSolucao),
 
     % O Mineiro está ao lado do jogador de 2,10 m.
-
+    aoLado(jogador(_,_,_,2.10,_,_), jogador(_,_,_,_,_,minasGerais), ListaSolucao),
 
     % Leandro está na terceira posição.
     Nome3 = leandro,
 
     % O jogador da região Centro-oeste está exatamente à esquerda do jogador da munhequeira Vermelha.
-
+    aoLado(jogador(_,_,_,_,_,matoGrossoDoSul), jogador(vermelha,_,_,_,_,_), ListaSolucao),
 
     % O jogador de 23 anos está em algum lugar entre o jogador da camisa 45 e o jogador mais novo, nessa ordem.
-
+    entre(jogador(_,_,45,_,_,_), jogador(_,_,45,_,_,_), jogador(_,_,20,_,_,_), ListaSolucao),
 
     % Tiago está exatamente à direita do jogador da camisa 6.
+    aoLado(jogador(_,tiago,_,_,_,_), jogador(_,_,6,_,_,_), ListaSolucao),
 
+    % Testa todas as possibilidades
+    munhequeira(Munhequeira1), munhequeira(Munhequeira2), munhequeira(Munhequeira3), munhequeira(Munhequeira4), munhequeira(Munhequeira5),
+    todosDiferentes([Munhequeira1, Munhequeira2, Munhequeira3, Munhequeira4, Munhequeira5]),
+
+    nome(Nome1), nome(Nome2), nome(Nome3), nome(Nome4), nome(Nome5),
+    todosDiferentes([Nome1, Nome2, Nome3, Nome4, Nome5]),
+
+    camisa(Camisa1), camisa(Camisa2), camisa(Camisa3), camisa(Camisa4), camisa(Camisa5),
+    todosDiferentes([Camisa1, Camisa2, Camisa3, Camisa4, Camisa5]),
+
+    altura(Altura1), altura(Altura2), altura(Altura3), altura(Altura4), altura(Altura5),
+    todosDiferentes([Altura1, Altura2, Altura3, Altura4, Altura5]),
+
+    idade(Idade1), idade(Idade2), idade(Idade3), idade(Idade4), idade(Idade5),
+    todosDiferentes([Idade1, Idade2, Idade3, Idade4, Idade5]),
+
+    estado(Estado1), estado(Estado2), estado(Estado3), estado(Estado4), estado(Estado5),
+    todosDiferentes([Estado1, Estado2, Estado3, Estado4, Estado5]).
